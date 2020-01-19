32c1e6c99e5c2396cd55ba2fac6d9d6384e59389
Jeong-Uk Park <dnr1105@gmail.com>
2020-01-19 23:24:00 +0900

    Add Components (Main TestUI)

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Test chage"/>
-  </div>
+    <div id="app">
+        <MainVue></MainVue>
+        <SenderVue></SenderVue>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import MainVue from './components/Main.vue'
+    import SenderVue from './components/Sender.vue'
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        components: {
+            'MainVue': MainVue,
+            'SenderVue' : SenderVue
+        }
+    }
 </script>
+<!--
+font-family: 'Black Han Sans', sans-serif;
+font-family: 'Nanum Pen Script', cursive;
+-->
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    #app {
+        /*font-family: 'Avenir', Helvetica, Arial, sans-serif;*/
+        font-family: 'Nanum Pen Script', cursive;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        left: 0;
+        right: 0;
+        height: 60%;
+        margin: auto;
+        /*top: 50%;*/
+        /*height: 100px;*/
+        /*margin-top: -50px;*/
+    }
 </style>
---
+++ src/components/Sender.vue
@@ -0,0 +1,80 @@
+<template>
+    <div id="wrapper">
+        <form method="post" action="">
+            KEY :
+            <input id="key" type="text" name="key">
+
+            <textarea
+                    placeholder="Enter something"
+                    id="value"
+                    name="value"
+                    rows="4"
+                    maxlength="100"
+                    style="overflow: hidden; word-wrap: break-word; resize: none; height: 160px; ">
+            </textarea>
+            <button id="button" type="submit">Send</button>
+        </form>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Sender"
+    }
+</script>
+
+<style scoped>
+    * {
+        font-family: 'Nanum Pen Script', cursive;
+        border: none;
+    }
+
+    #key {
+        border-bottom: 2px solid #222222;
+        color: #000000;
+        font-size: 20px;
+    }
+
+    #value {
+        background: #a3cca3;
+        width: 90%;
+        padding: 40px;
+        color: #000;
+        font-size: 24px;
+        resize: none;
+        line-height: 40px;
+        margin: 10px;
+        border-radius: 20px;
+        box-shadow: 3px 3px 10px #6e6e6e;
+    }
+
+    #button {
+        float: right;
+        height: 40px;
+        padding: 0 24px;
+        font-size: 25px;
+        color: #FFF;
+        border-radius: 5px;
+        box-shadow: 3px 3px 10px #6e6e6e;
+        background: linear-gradient(to right, #6478FB, #8763FB);
+    }
+
+    #button:active {
+        zoom: 1;
+        filter: alpha(opacity=80);
+        opacity: 0.8;
+    }
+
+    #button:focus {
+        zoom: 1;
+        filter: alpha(opacity=80);
+        opacity: 0.8;
+    }
+
+    #wrapper {
+        width: 700px;
+        height: auto;
+        margin-left: auto;
+        margin-right: auto;
+    }
+</style>
