f9ebd72ecd6c1930fdc249748df32f51e5665194
Jeong-Uk Park <dnr1105@gmail.com>
2020-01-23 01:44:40 +0900

    Update & Add components

---
+++ src/components/TemporageRepo.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        Here is repo
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "TemporageRepo"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
 
-Vue.config.productionTip = false
+// import TsHeader from './components/TemporageHeader'
+import TsLogin from './components/TemporageLogin'
+import TsSender from './components/TemporageSender'
+import TsRepo from './components/TemporageRepo'
+
+Vue.use(VueRouter);
+Vue.config.productionTip = false;
+
+var router = new VueRouter({
+  mode: 'history',
+  routes :[
+    { path: '/login', component: TsLogin },
+    { path: '/', component: TsSender },
+    { path: '/repo', component: TsRepo },
+  ]
+});
 
 new Vue({
+  router,
   render: h => h(App),
-}).$mount('#app')
+}).$mount('#app');
