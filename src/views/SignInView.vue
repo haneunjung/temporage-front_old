ad70bf65c8ab0e722879bb23cfe2658cf097c1b6
Jeong-Uk Park <dnr1105@gmail.com>
2020-03-05 00:48:26 +0900

    Add 2 views
    signin, signup

---
+++ src/views/SignInView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        SignInView
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "SignInView"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/views/SignUpView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        SignUpView
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "SignUpView"
+    }
+</script>
+
+<style scoped>
+
+</style>
