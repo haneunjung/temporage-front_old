0f70a6189735a765a0bcaa4e4c149e21846979a1
Jeong-Uk Park <dnr1105@gmail.com>
2020-03-05 20:21:11 +0900

    Add routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,26 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 
+import MainView from "@/views/MainView";
+import SignUpView from "@/views/SignUpView";
+import SignInView from "@/views/SignInView";
+
 Vue.use( VueRouter );
 
 export const router = new VueRouter( {
     mode: 'history',
-    routes: {},
+    routes: [
+        {
+            path: '/',
+            component: MainView,
+        },
+        {
+            path: '/register',
+            component: SignUpView,
+        },
+        {
+            path: '/login',
+            component: SignInView,
+        },
+    ],
 } );
