97eb9737e979e29d70c9f2b2caec20fc1afda3fe
Jeong-Uk Park <dnr1105@gmail.com>
2020-03-06 00:44:25 +0900

    Add Vuetify
    - `npm i Vuetify`
    - Add plugins/vuetify/index.js
    - Add webpack.config.js

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue';
 
 import { router } from '@/router';
 import { store } from '@/store';
+import { vuetify } from "@/plugins/vuetify";
 
 Vue.config.productionTip = false;
 
@@ -10,4 +11,5 @@ new Vue( {
     render: h => h( App ),
     router,
     store,
+    vuetify,
 } ).$mount( '#app' );
